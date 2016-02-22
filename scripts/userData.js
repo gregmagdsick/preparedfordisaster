a52db604543bcf5caf7697c1a1bc75c5f1f195f7
Rick Stoner <stonerrick19@gmail.com>
2016-02-22 10:51:31 -0800

    added functions to pull, save, and erase information from firebase, added object key for user information

---
+++ scripts/userData.js
@@ -0,0 +1,40 @@
+(function(module) {
+  'use strict';
+//***test variables and functions, to be deleted***
+
+
+
+// ***end delete test section***
+  CurrentUser.all = [];
+  //creates a new object for the user with properties to match data input
+  function CurrentUser(ops) {
+    Object.keys(ops).forEach(function(e, index, keys) {
+      this[e] = ops[e];
+    },this);
+  }
+  //pulls data from Firebase if the user has an id
+  CurrentUser.pullData = function(userId) {
+    var userInfo = new Firebase('https://blinding-fire-6623.firebaseio.com/web/data/' + userId);
+    userInfo.on('value', function(snapshot){
+      CurrentUser.all = new CurrentUser(snapshot.val());
+    }, function(error){
+      console.log('Read failed', error);
+    });
+  };
+  //Saves data to Firebase, will overwrite any existing data for the specific user.
+  CurrentUser.saveData = function(userId) {
+    var userInfo = new Firebase('https://blinding-fire-6623.firebaseio.com/web/data/');
+    var storeData = userInfo.child(userId);
+    storeData.set(CurrentUser.all);
+  };
+  //erases the data in firebase for the specific user
+  CurrentUser.eraseData = function(userId) {
+    if(userId){
+      var userData = new Firebase('https://blinding-fire-6623.firebaseio.com/web/data/' + userId);
+      userData.remove();
+    }
+  };
+
+
+  module.CurrentUser = CurrentUser;
+}(window));
