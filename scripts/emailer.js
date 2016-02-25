202f1f218d3f4b0f9dce1946c5bc6d6f67c92613
Joseph McClenahan <jmcclena94@gmail.com>
2016-02-25 13:43:17 -0800

    editing server.js with socket.io

---
+++ scripts/emailer.js
@@ -0,0 +1,14 @@
+$('#authorize-button').on('click',function() {
+
+  // setup e-mail data with unicode symbols
+  var mailOptions = {
+    from: 'jmcclena94@gmail.com', // sender address
+    to: 'jmcclena94@gmail.com', // list of receivers
+    subject: 'test1', // Subject line
+    text: 'test1', // plaintext body
+    html: '' // html body
+  };
+
+  var socket = io('http://localhost');
+  socket.emit('emailResponse',mailOptions);
+})
--- server.js
+++ server.js
@@ -13,3 +13,54 @@ app.get('*', function(request, response) {
 app.listen(port, function() {
   console.log('Server started on port ' + port + '!');
 });
+
+var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+var io = require('socket.io')();
+
+var options = {
+    service: 'gmail',
+    auth: {
+        user: 'info.preparedfordisaster',
+        pass: 'Code16Disaster301'
+    }
+  };
+var transporter = nodemailer.createTransport(smtpTransport(options));
+
+io.on('connection', function(socket) {
+  socket.on('emailResponse', function (mailOptions) {
+
+    transporter.sendMail(mailOptions, function(error, response){
+        if(error){
+            console.log(error);
+        }else{
+            console.log("Message sent: " + response.message);
+        }
+
+        // if you don't want to use this transport object anymore, uncomment following line
+        // smtpTransport.close(); // shut down the connection pool, no more messages
+    });
+  });
+});
+//
+
+  // setup e-mail data with unicode symbols
+  // var mailOptions = {
+  //   from: 'jmcclena94@gmail.com', // sender address
+  //   to: 'jmcclena94@gmail.com', // list of receivers
+  //   subject: 'test1', // Subject line
+  //   text: 'test1', // plaintext body
+  //   html: '' // html body
+  // };
+  //
+  // // send mail with defined transport object
+  // transporter.sendMail(mailOptions, function(error, response){
+  //     if(error){
+  //         console.log(error);
+  //     }else{
+  //         console.log("Message sent: " + response.message);
+  //     }
+  //
+  //     // if you don't want to use this transport object anymore, uncomment following line
+  //     // smtpTransport.close(); // shut down the connection pool, no more messages
+  // });
