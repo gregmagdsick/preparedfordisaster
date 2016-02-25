0bec44b2ee5274071eddde473733aa4876f3d71b
Phil Nguyen <philnguyen@Phils-MacBook-Pro.local>
2016-02-25 12:53:16 -0800

    add style to templates

--- scripts/base64test.js
+++ scripts/base64test.js
@@ -1,21 +1,22 @@
 (function(module) {
-  var base64test= {};
+    var base64test = {};
 
-  function encodeImageFileAsURL(cb) {
-    return function() {
-      var file = this.files[0];
-      var reader = new FileReader();
-      reader.onloadend = function() {
-        cb(reader.result);
+    function encodeImageFileAsURL(cb) {
+      return function() {
+        var file = this.files[0];
+        var reader = new FileReader();
+        reader.onloadend = function() {
+          cb(reader.result);
+        };
+        reader.readAsDataURL(file);
       };
-      reader.readAsDataURL(file);
-    };
-  }
+    }
 
-  base64test.handle = function () {
-    $('#inputFileToLoad').change(encodeImageFileAsURL(function(base64Img) {
-      console.log(base64Img);
-      $('#emergenecy-info').find('img').attr('src', base64Img)});
-  };
-    module.base64test = base64test;
-})(window);
+    base64test.handle = function() {
+      $('#inputFileToLoad').change(encodeImageFileAsURL(function(base64Img) {
+          console.log(base64Img);
+          $('#emergenecy-info').find('img').attr('src', base64Img);
+        }));
+      };
+      module.base64test = base64test;
+    })(window);
