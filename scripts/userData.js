eb154304bb9a1c35d83ec3f119c75fc1c14c1eb5
Rick Stoner <stonerrick19@gmail.com>
2016-02-25 16:52:53 -0800

    added message to let user the submission was successful, final submit button takes user to home page

--- scripts/userData.js
+++ scripts/userData.js
@@ -20,7 +20,8 @@
         CurrentUser.all = new CurrentUser(snapshot.val());
         $('#homepage-plan').append(home.render(CurrentUser.all, '#final-emergency-plan-form-template'));
         CurrentUser.all.lovedOnes.forEach(function(a) {$('#homepage-plan').append(home.render(a, '#final-emergency-plan-lovedones-template'));
-      });
+        });
+
         $('#homepage-plan').append(home.render(CurrentUser.all, '#final-emergency-plan-rally-template'));
         $('#homepage-plan').append(home.render(CurrentUser.all, '#final-emergency-plan-userkit-template'));
       }
